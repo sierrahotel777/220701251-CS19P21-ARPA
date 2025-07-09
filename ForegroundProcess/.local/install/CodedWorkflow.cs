17ebc6235b15d5b174feb190c9e629429fcc86b0
sierrahotel777 <sierrahotel777@gmail.com>
2025-07-09 08:18:45 +0530

    Added Foreground Process

---
+++ ForegroundProcess/.local/install/CodedWorkflow.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace ForegroundProcess
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService)};
+        }
+
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
+
+        protected UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService uiAutomation { get => serviceContainer.Resolve<UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService>() ; }
+    }
+}
