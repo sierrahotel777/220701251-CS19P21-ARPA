a1127e70e69e9cb6ac718a75d260d4ec59db66d3
sierrahotel777 <sierrahotel777@gmail.com>
2025-08-04 13:32:09 +0530

    Uploaded Exp 3,5,7

--- 05 Type Into/.local/install/CodedWorkflow.cs
+++ 05 Type Into/.local/install/CodedWorkflow.cs
@@ -14,13 +14,13 @@ using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
 using UiPath.CodedWorkflows.DescriptorIntegration;
 
-namespace _02VersionControl
+namespace AppDemo
 {
     public partial class CodedWorkflow : CodedWorkflowBase
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
@@ -31,14 +31,14 @@ namespace _02VersionControl
     }
 }
 
-namespace _02VersionControl.ObjectRepository
+namespace AppDemo.ObjectRepository
 {
     public static class Descriptors
     {
     }
 }
 
-namespace _02VersionControl._Implementation
+namespace AppDemo._Implementation
 {
     internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
     {
